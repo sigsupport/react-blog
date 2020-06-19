bf165dd0ce41f0da6122e1d33329fce1bf05f7b4
glewis <Geoffrey.Lewis@wacom.com>
2020-06-19 16:38:33 +0100

    First commit

---
+++ src/build/precache-manifest.191a5337bbd3d0f845394d12d83d23b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d762be69a2a4580b5f533a4c1f21894c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5978bc1477331af5d0",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "82338777957a3a136d00",
+    "url": "/static/js/2.1dce0312.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1dce0312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5978bc1477331af5d0",
+    "url": "/static/js/main.20cafd22.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
